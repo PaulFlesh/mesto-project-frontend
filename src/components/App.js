9017d28daf322e07014c858c9023a4361991ec69
Johnny Blade <notjohnnyblade@yandex.ru>
2024-02-04 17:52:36 +0400

    test

--- src/components/App.js
+++ src/components/App.js
@@ -54,7 +54,7 @@ function App() {
             history.push("/");
         })
         .catch((err) => {
-          localStorage.removeItem("jwt");
+          //localStorage.removeItem("jwt");
           console.log(err);
         });
     }
